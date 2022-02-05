78fe14002ef3b77b16bc92943d1cf1eb42599652
Raffo98 <raf.riccardelli@gmail.com>
2022-02-05 16:42:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
     },
@@ -28,7 +28,7 @@ const app = Vue.createApp({
             return this.variants[this.selectedVariant].image
         },
         inStock() {
-            return this.variants[this.selectedVariant].image
-        }
+            return this.variants[this.selectedVariant].quantity
+        },
     }
 })
